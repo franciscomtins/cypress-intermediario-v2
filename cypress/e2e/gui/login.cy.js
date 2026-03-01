70353f624a839fa91a915491289778ec77c4a14b
Francisco Martins <francisco.mtins@gmal.com>
2026-03-01 10:27:57 -0300

    Primeiro commit - refazendo curso intermediario de cypress - v2

---
+++ cypress/e2e/gui/login.cy.js
@@ -0,0 +1,12 @@
+describe('login', ()=> {
+    it('successfully', () =>{
+        const user = Cypress.env('user_name')
+        const password = Cypress.env('user_password')
+        const options = {cacheSession: false}
+
+        cy.login(user, password, options)
+        cy.visit('/')
+
+        cy.get('.qa-user-avatar').should('be.visible')
+    })
+})
